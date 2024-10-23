f901326ae486d3ee1bcfeae59fc543853fcc8146
nataliamatulis <nati.matulis@gmail.com>
2024-10-23 19:46:39 -0300

    Taller

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.getElementById('buttonText').addEventListener('click', function() {
     const inputText = document.getElementById('inputText').value;
     localStorage.setItem('inputData', inputText);
+    alert('Dato guardado: ' + inputText); // Confirmación de guardado
 });
